fdcd2a5b26663c34d3560c30c1c61184c35c8226
Admin <Admin@PCJAN>
2025-11-28 10:58:55 +0100

    Up

---
+++ GIMINI/1/zad 2 weryfikacja hasla/zad 2 weryfikacja hasla/Źródło.cpp
@@ -0,0 +1,60 @@
+//#include <iostream>
+//#include <string>
+
+//using namespace std;
+//
+//int main()
+//{
+//	string haslo;
+//	cout << "Podaj haslo : ";
+//	cin >> haslo;
+//	cout << endl;
+//	if (haslo == "C++Teoria")
+//	{
+//		cout << "Dostep przyznany. Witamy w systemie!";
+//	}
+//	else
+//	{
+//		cout << "Bledne haslo. Dostep zabroniony";
+//	}
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	int punkty;
+//	cout << "Podaj liczbe punktow od 0 do 100: ";
+//	cin >> punkty;
+//	if (punkty >= 80 && punkty <= 100)
+//		cout << "Bardzo dobrze (5)";
+//	else if (punkty >= 60 && punkty <= 79)
+//		cout << "Dobrze (4)";
+//	else if (punkty >= 50 && punkty <= 59)
+//		cout << "Dostatecznie (3)";
+//	else if (punkty > 100)
+//		cout << "Poza zakresem";
+//	else
+//		cout << "Niedostatecznie (2)";
+//}
+
+//using namespace std;
+//
+//int main()
+//{ 
+//	double ilosc_godzin, oplata=5, oplata_2=3, oplata_o;
+//
+//	cout << "Podaj liczbe godzin ile bedziesz na naszym parkingu :";
+//	cin >> ilosc_godzin;
+//	if (ilosc_godzin == 1)
+//		cout << "Oplata za " << ilosc_godzin << " godzine wynosi : " << oplata << "PLN";
+//	else
+//	{
+//		oplata_o = oplata + ((ilosc_godzin - 1) * oplata_2);
+//		cout << "Oplata za " << ilosc_godzin << " godzine wynosi : " << oplata_o << "PLN";
+//	}
+//	return 0;
+//}
